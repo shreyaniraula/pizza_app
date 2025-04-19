ae8b30a9a2a56b525e88cc1362e750a0526500ca
Shreya <shreeya.775421@mmamc.tu.edu.np>
2025-04-19 09:30:08 +0545

    Entities and models created, user getter defined

--- packages/user_repository/lib/src/firebase_user_repo.dart
+++ packages/user_repository/lib/src/firebase_user_repo.dart
@@ -1,40 +1,57 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:rxdart/transformers.dart';
+import 'package:user_repository/src/models/user.dart';
 import 'package:user_repository/src/user_repo.dart';
+import 'package:user_repository/user_repository.dart';
 
 class FirebaseUserRepo implements UserRepository {
   final FirebaseAuth _firebaseAuth;
   final usersCollection = FirebaseFirestore.instance.collection('users');
 
-  FirebaseUserRepo({
-    FirebaseAuth? firebaseAuth,
-  }): _firebaseAuth = firebaseAuth ?? FirebaseAuth.instance;
-  
+  FirebaseUserRepo({FirebaseAuth? firebaseAuth})
+    : _firebaseAuth = firebaseAuth ?? FirebaseAuth.instance;
+
   @override
-  Future<void> logOut() {
-    // TODO: implement logOut
-    throw UnimplementedError();
+  // TODO: implement user
+  Stream<MyUser?> get user {
+    return _firebaseAuth.authStateChanges().flatMap((firebaseUser) async* {
+      if (firebaseUser == null) {
+        yield MyUser.empty;
+      } else {
+        //access the document from firebase with userId, transform it to MyUser object
+        yield await usersCollection
+            .doc(firebaseUser.uid)
+            .get()
+            .then(
+              (value) =>
+                  MyUser.fromEntity(MyUserEntity.fromDocument(value.data()!)),
+            );
+      }
+    });
   }
-  
-  @override
-  Future<void> setUserData(user) {
-    // TODO: implement setUserData
-    throw UnimplementedError();
-  }
-  
+
   @override
   Future<void> signIn(String email, String password) {
     // TODO: implement signIn
     throw UnimplementedError();
   }
-  
+
   @override
-  Future<dynamic> signUp(user, String password) {
+  Future<MyUser> signUp(MyUser user, String password) {
     // TODO: implement signUp
     throw UnimplementedError();
   }
-  
+
   @override
-  // TODO: implement user
-  Stream<dynamic> get user => throw UnimplementedError();
+  Future<void> logOut() {
+    // TODO: implement logOut
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> setUserData(MyUser user) {
+    // TODO: implement setUserData
+    throw UnimplementedError();
+  }
 }
