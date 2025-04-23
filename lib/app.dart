76cd3f42e9a068d294e15e9661af7505fa111783
Shreya <shreeya.775421@mmamc.tu.edu.np>
2025-04-23 18:28:00 +0545

    Basic configurations made

--- lib/app.dart
+++ lib/app.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:user_repository/user_repository.dart';
 
+import 'app_view.dart';
 import 'blocs/authentication_bloc/authentication_bloc.dart';
 
 class MyApp extends StatelessWidget {
